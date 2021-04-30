25b5b19192874616377d762d4cf332d57198f532
anton <melnikov1994@gmail.com>
2021-04-30 18:59:05 +0300

    added player movment

---
+++ Assets/playerMovment.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovment : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public float speed = 12f;
+    private Camera mainCam;
+    private CharacterController controller;
+    private float xRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = Camera.main;
+        Cursor.lockState = CursorLockMode.Locked;
+        controller = transform.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        mainCam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
