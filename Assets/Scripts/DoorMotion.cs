e232abb15ac5818a0ea01ab1ef1de0bd5ab96b7b
AdirKoryat <koryatadir@gmail.com>
2021-05-01 13:06:38 +0300

    adir

--- Assets/Scripts/DoorMotion.cs
+++ Assets/Scripts/DoorMotion.cs
@@ -13,18 +13,23 @@ public class DoorMotion : MonoBehaviour
     void Start()
     {
         anim = transform.gameObject.GetComponent<Animator>();
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
         anim.SetBool("isOpen", true);
-        open.PlayDelayed(0.7f);
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !anim.IsInTransition(0))
+            open.Play();
+        //open.PlayDelayed(0.7f);
     }
 
     private void OnTriggerExit(Collider other)
     {
         anim.SetBool("isOpen", false);
-        close.PlayDelayed(0.7f);
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !anim.IsInTransition(0))
+            close.Play();
+        //close.PlayDelayed(0.7f);
     }
 
     // Update is called once per frame
