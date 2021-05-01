f6a4fbd6462366834a3a0f1da39748df84f585c1
Vithralas <47637471+VithralasOfficial@users.noreply.github.com>
2021-05-01 18:22:05 +0300

    Player Sound And Bench Sit

--- Assets/Scripts/playerMovment.cs
+++ Assets/Scripts/playerMovment.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerMovment : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
     public float speed = 12f;
     public float gravity = -9.8f;
+    public float distance = 4f;
+    public Text message;
     private Camera mainCam;
     private CharacterController controller;
     private float xRotation = 0f;
     private Vector3 velocity;
+    private AudioSource footSteps;
+    private Vector3 lastPos;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +23,30 @@ public class playerMovment : MonoBehaviour
         mainCam = Camera.main;
         Cursor.lockState = CursorLockMode.Locked;
         controller = transform.GetComponent<CharacterController>();
+        footSteps = transform.GetComponent<AudioSource>();
+        lastPos = transform.position;
+    }
+
+    void checkInteraction()
+    {
+        message.text = "";
+        Vector3 origin = mainCam.transform.position;
+        Vector3 direction = mainCam.transform.forward;
+        RaycastHit hit;
+        if (Physics.Raycast(origin, direction, out hit, distance))
+        {
+            if (hit.transform.tag == "bench")
+            {
+                message.text = "Press F to sit on bench";
+
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    transform.position = new Vector3(hit.transform.position.x - 0.5f, transform.position.y + 1
+                        , hit.transform.position.z);
+                    transform.Rotate(0, 180 - transform.rotation.y, 0, Space.World);
+                }
+            }
+        }
     }
 
     // Update is called once per frame
@@ -38,5 +67,15 @@ public class playerMovment : MonoBehaviour
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+
+        if (transform.position.x != lastPos.x || transform.position.z != lastPos.z)
+        {
+            lastPos = transform.position;
+            if (!footSteps.isPlaying)
+                footSteps.Play();
+        }
+
+
+        checkInteraction();
     }
 }
